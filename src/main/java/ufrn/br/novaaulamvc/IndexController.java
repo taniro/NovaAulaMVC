95dfa11aba29436135582f31ae3ecf252030d9fa
tanirocr <tanirocr@gmail.com>
2024-04-11 09:26:12 -0300

    aula

--- src/main/java/ufrn/br/novaaulamvc/IndexController.java
+++ src/main/java/ufrn/br/novaaulamvc/IndexController.java
@@ -1,2 +1,55 @@
-package ufrn.br.novaaulamvc;public class IndexController {
+package ufrn.br.novaaulamvc;
+
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = {"/", "/index.html"}, method = RequestMethod.GET)
+    public String getIndexPage(Model model){
+
+        List<Pessoa> nomes = new ArrayList<>();
+        nomes.add(new Pessoa("Pessoa 1", 30));
+        nomes.add(new Pessoa("Pessoa 2", 20));
+        nomes.add(new Pessoa("Pessoa 3", 40));
+
+
+        String nome = "Taniro";
+
+        model.addAttribute("objeto", nome);
+        model.addAttribute("pessoas", nomes);
+
+        return "index";
+    }
+
+
+    @RequestMapping(value = "/editar/{id}", method = RequestMethod.GET)
+    public String editarPage(@PathVariable Integer id, Model model){
+
+        model.addAttribute("size", id);
+
+        return "editar";
+    }
+
+    @GetMapping("/cadastrar")
+    public String getCadastroPage(Model model){
+
+        Pessoa p = new Pessoa("pessoa", 20);
+        model.addAttribute("p", p);
+        return "cadastrar";
+    }
+    @PostMapping("/salvar")
+    public String doSalvar(@ModelAttribute Pessoa p){
+
+        System.out.println(p.toString());
+        return "redirect:/";
+    }
+
 }
--- src/test/java/ufrn/br/novaaulamvc/NovaAulaMvcApplicationTests.java
+++ src/test/java/ufrn/br/novaaulamvc/NovaAulaMvcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NovaAulaMvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
